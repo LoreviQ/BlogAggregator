80814126425b7b6d753c62eae9f55002bfd641e8
LoreviQ <lorevi@protonmail.com>
2024-03-26 23:02:50 +0000

    Added functions to initialise and start server

--- main.go
+++ main.go
@@ -1,3 +1,46 @@
 package main
 
-func main()
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type ApiConfig struct {
+	port string
+}
+
+func main() {
+	godotenv.Load()
+	cfg := ApiConfig{
+		port: os.Getenv("PORT"),
+	}
+	server := initialiseServer(cfg, http.NewServeMux())
+
+	log.Printf("Serving on port: %s\n", cfg.port)
+	log.Panic(server.ListenAndServe())
+}
+
+func initialiseServer(cfg ApiConfig, mux *http.ServeMux) *http.Server {
+	corsMux := cfg.CorsMiddleware(mux)
+	server := &http.Server{
+		Addr:    ":" + cfg.port,
+		Handler: corsMux,
+	}
+	return server
+}
+
+func (cfg *ApiConfig) CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
